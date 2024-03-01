28af2496e633ca4f2b88d32c9966a1105d8cb6c4
blast <fberrzig@gmail.com>
2024-03-01 01:00:17 +0100

    fixed some

--- controller/2glance/1_0glance-in-keystone.sh
+++ controller/2glance/1_0glance-in-keystone.sh
@@ -3,9 +3,9 @@ openstack user create --domain default --project service --password servicepassw
 openstack role add --project service --user glance admin
 openstack service create --name glance --description "OpenStack Image service" image
 export controller=192.168.1.11
-openstack endpoint create --region RegionOne image public https://$controller:9292
-openstack endpoint create --region RegionOne image internal https://$controller:9292
-openstack endpoint create --region RegionOne image admin https://$controller:9292
+openstack endpoint create --region RegionOne image public http://$controller:9292
+openstack endpoint create --region RegionOne image internal http://$controller:9292
+openstack endpoint create --region RegionOne image admin http://$controller:9292
 mysql
 
 apt -y install glance
